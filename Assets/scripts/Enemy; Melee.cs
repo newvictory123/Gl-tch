f3911546e39c541be4c9989d04679ce609c49988
GedeGustav <gustavsvare@gmail.com>
2024-02-19 22:24:37 +0100

    we chilling?

---
+++ Assets/scripts/Enemy; Melee.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.ProBuilder;
+
+public class Enemy_Melee : MonoBehaviour
+{
+    public GameObject player;
+    public CharacterController controller;
+    public Vector3 moveDirection;
+    public ParticleSystem ps;
+    public MeshRenderer mR;
+    public Transform[] movePoints;
+
+    [Header("Stats")]
+    public float moveSpeed;
+    public float visionRange;
+    public float visionAngle;
+    public float deathRange;
+    private int indexOfTarget;
+    private Vector3 targetPosition;
+
+    private State state = State.Patrol;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        controller = GetComponent<CharacterController>();
+        ps = GameObject.Find("Sparks").GetComponent<ParticleSystem>();
+        mR = GetComponent<MeshRenderer>();
+        indexOfTarget = -1;
+        NextTarget();
+        LookAtTarget();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveDirection = player.transform.position - controller.transform.position;
+        moveDirection = new Vector3(moveDirection.x, 0, moveDirection.z).normalized;
+
+        switch (state)
+        {
+            case State.Patrol:
+                Patrol();
+                break;
+            case State.Chase:
+                Chase();
+                break;
+        }
+    }
+
+
+    IEnumerator Exsplode()
+    {
+
+        //yield return new WaitForSeconds(1f);
+
+        mR.enabled = false;
+
+        ps.Play();
+
+        yield return new WaitForSeconds(2f);
+
+        Destroy(gameObject);
+
+        yield return null;
+    }
+
+
+
+    void LookAtPlayer()
+    {
+        Vector3 lookAtPlayer = player.transform.position;
+        lookAtPlayer.y = transform.position.y;
+
+        Vector3 lookToPlayerDirection = (lookAtPlayer - transform.position).normalized;
+        transform.forward = lookToPlayerDirection;
+    }
+
+    float GetDistanceToPlayer()
+    {
+        return
+            (player.transform.position - transform.position)
+            .magnitude;
+    }
+
+    float GetAngleToPlayer()
+    {
+        Vector3 directionToPlayer =
+            (player.transform.position - transform.position)
+            .normalized;
+        return Vector3.Angle(transform.forward, directionToPlayer);
+    }
+
+    bool SightLineObstructed()
+    {
+        Vector3 vectorToPlayer = player.transform.position - transform.position;
+        Ray ray = new Ray(
+            transform.position,
+            vectorToPlayer);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, vectorToPlayer.magnitude))
+        {
+            GameObject obj = hitInfo.collider.gameObject;
+            return obj != player;
+        }
+        return false;
+    }
+
+    bool CanSeePlayer()
+    {
+        if (GetDistanceToPlayer() < visionRange && !SightLineObstructed() && GetAngleToPlayer() < visionAngle)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void NextTarget()
+    {
+        indexOfTarget = (indexOfTarget + 1) % movePoints.Length;
+        targetPosition = movePoints[indexOfTarget].position;
+        targetPosition.y = transform.position.y;
+    }
+
+    void LookAtTarget()
+    {
+        Vector3 lookAt = targetPosition;
+        lookAt.y = transform.position.y;
+
+        Vector3 lookDir = (lookAt - transform.position).normalized;
+        transform.forward = lookDir;
+    }
+
+    void Patrol()
+    {
+        if (CanSeePlayer())
+        {
+            state = State.Chase;
+        }
+
+        if (agent.remainingDistance < agent.radius)
+        {
+            NextTarget();
+        }
+        agent.SetDestination(targetPosition);
+    }
+
+
+    void Chase()
+    {
+        if (GetDistanceToPlayer() < deathRange)
+        {
+            StartCoroutine(Exsplode());
+        }
+
+        if (!CanSeePlayer())
+        {
+            state = State.Patrol;
+        }
+
+        LookAtPlayer();
+        Vector3 velocity = player.transform.position - transform.position;
+        velocity.Normalize();
+        velocity *= moveSpeed * Time.deltaTime;
+        controller.Move(velocity);
+
+        if ((transform.position - player.transform.position).magnitude > visionRange)
+        {
+            state = State.Patrol;
+            LookAtTarget();
+        }
+    }
+
+
+
+
+    enum State
+    {
+        Patrol,
+        Chase,
+    }
+
+
+}
